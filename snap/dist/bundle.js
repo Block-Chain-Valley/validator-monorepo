2f2601e7500316b813fd80599c66b50f57a728a4
everGreenGH <skin0011@naver.com>
2022-11-26 09:39:11 +0900

    feat: 로고 수정

--- snap/dist/bundle.js
+++ snap/dist/bundle.js
@@ -70,7 +70,7 @@
           params: [{
             prompt: "Hello, User!",
             description: "Validator를 설치해 주셔서 감사합니다.",
-            textAreaContent: "Validator는 Snap을 기반으로 한, 컨트랙트 피싱 방지 서비스입니다. \nVersion test1.2"
+            textAreaContent: "Validator는 Snap을 기반으로 한, 컨트랙트 피싱 방지 서비스입니다. \nVersion test1.3"
           }]
         });
       };
